adab426a810dec4d2cdf6c9618578b316199016a
Atul R <atulanand94@gmail.com>
2020-02-15 23:52:44 +0100

    Adds working qt run looper

--- src/demo.ts
+++ src/demo.ts
@@ -3,18 +3,6 @@ import { QWidget } from './lib/QtWidgets/QWidget';
 import { FlexLayout } from './lib/core/FlexLayout';
 import { QTableWidget } from './lib/QtWidgets/QTableWidget';
 
-// the code commented below have no problem
-/*const view = new QTableView();
-
-view.setStyleSheet(`
-QTableView {
-    min-width:300px;
-    min-height:200px;
-}
-`);
-view.show();
-*/
-
 const widget = new QWidget();
 const layout = new FlexLayout();
 const view = new QTableView();
@@ -35,12 +23,11 @@ QTableView {
 `);
 
 widget.setLayout(layout);
-layout.addWidget(view); // app crash when add view to the layout
-layout.addWidget(table); // app run ok when add table to the layout
+layout.addWidget(view);
+layout.addWidget(table);
 
 widget.setInlineStyle('flex:1;');
 widget.resize(600, 400);
 widget.show();
 
-//(global as any).main = view;
 (global as any).main = widget;
